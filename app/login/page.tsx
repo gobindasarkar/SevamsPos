2da001ef2bcde1ffac169618ee6f445b6ca95912
gobinda sarkar <55000159+gobindasarkar@users.noreply.github.com>
2025-09-02 17:32:28 +0530

    banner slider add

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
-import Logo from '@/public/sevampos-logo.png'
 import sevam from '@/public/sevam.png'
 import LoginForm from "@/components/LoginForm";
+import BannderSlider from "@/components/BannderSlider";
 export default function page() {
   return (
     <div className="grid place-items-center min-h-screen p-4 bg-color1">
       <div className="max-w-xl w-full space-y-6">
-        <div className="mb-5 text-center">
-          <Image className="max-w-64 mx-auto object-cover mb-5" src={Logo} alt="Sevams Pos" />
-          <h4 className="text-muted-foreground">Fine Dining Experience</h4>
+        <div className="mb-5 w-full max-w-md mx-auto overflow-hidden relative">
+          <BannderSlider />
         </div>
         <div className="rounded-lg border text-bcolor shadow-sm p-6 w-full max-w-md mx-auto bg-[#0f131a] border-bordercolor shadow-blue ">
           <div className="space-y-6">
---
+++ components/BannderSlider.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
+import Slider, { Settings } from "react-slick";
+import Image from "next/image";
+
+type Slide = {
+    id: number;
+    src: string;
+    alt: string;
+};
+const slides: Slide[] = [
+    { id: 1, src: "/hero-banner-1.jpg", alt: "Slide 1" },
+    { id: 2, src: "/hero-banner-2.jpg", alt: "Slide 2" },
+];
+
+export default function BannerSlider() {
+    const settings: Settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+    };
+
+    return (
+        <Slider {...settings}>
+            {slides.map((slide) => (
+                <div key={slide.id} className="relative w-[100%] h-[200px]">
+                    <Image
+                        src={slide.src}
+                        alt={slide.alt}
+                        fill
+                        priority
+                        className="object-cover rounded-xl w-full h-full"
+                    />
+                </div>
+            ))}
+        </Slider>
+    );
+}
